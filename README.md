b5c9239d8140c95a33738a60bfccb884fe7df953
Jonathan Cushing <firex42069@gmail.com>
2022-04-03 23:15:07 -0400

    readme; js note

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+![Screenshot of the work day scheduler](./assets/images/screenshot.png)
+
+## Purpose
+This work day scheduler features nine rows for each hour of the average work day. Text can be entered into each row and can be saved by clicking the respective button on the right. Each row also changes its color depending on if the hour has already passed or is currently passing.
+
+Well, it *could* save and it *could* change color if I implemented the JS for it.
+
+## Website
+https://jec6789.github.io/work-day-scheduler/
+
+## Contributors
+* HTML and CSS by Xandromus
+* Additional HTML by JEC6789
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-// something
+/* 
+In my coding bootcamp, the lowest two scores I have are automatically dropped when it ends in August, effectively
+allowing me to skip two different assignments for any reason. I think I'm gonna make this one of them.
+
+Moment.js and jQuery are both old technologies that have become obsolete in the modern Internet era.
+Polymer or React can be used in place of jQuery, and Moment.js was officially deprecated in 2020.
+
+Additionally, due to circumstances beyond my control, the amount of free time I have this weekend has been
+greatly reduced, so I feel like this would be one of the two assignments that's best to skip for me.
+Hopefully this is not a decision I will regret.
+*/
